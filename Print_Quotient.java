a3f2d4bc5d096c921b9bdf13308268e814c98b28
Vishal Agarwal <vagarwal102502@gmail.com>
2019-06-11 11:34:22 -0400

    Runner and Interface for Print_Quotient created

---
+++ Print_Quotient.java
@@ -0,0 +1,3 @@
+public interface Print_Quotient {
+    public float printQuotient(float f1, float f2);
+}
--- Runner.java
+++ Runner.java
@@ -1,7 +1,13 @@
 public class Runner{
 	public static void main(String[] args) 
 	{
+	    int x = 10;
 
+        int y = 5;
+
+        Print_Quotient pQ = (float x,y)->System.out.println(x/y);
+
+        System.out.printf("%.3f",pQ(x,y));
 		
 	} 
 }  
